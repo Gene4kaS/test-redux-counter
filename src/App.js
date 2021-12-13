d538dba4df3a69967c906a9013520f52761fbd01
Gene4kaS <jannasavchenko@gmail.com>
2021-12-13 23:16:00 +0200

    tasks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import './App.css';
+import React from 'react';
+import Counter from './components/Counter/Counter';
+import Dropdown from './components/Dropdown/Dropdown';
+import ColorPicker from './components/ColorPicker/ColorPicker';
 
-function App() {
-  return <div className="App">TEST</div>;
-}
+const colorPickerOptions = [
+  { label: 'red', color: '#F44336' },
+  { label: 'green', color: '#4CAF50' },
+  { label: 'blue', color: '#2196F3' },
+  { label: 'grey', color: '#607D8B' },
+  { label: 'pink', color: '#E91E63' },
+  { label: 'indigo', color: '#3F51B5' },
+];
+
+const App = () => (
+  <>
+    <h1>Состояние компонента</h1>
+    <Counter initialValue={0} />
+    <Dropdown />
+    <ColorPicker options={colorPickerOptions} />
+  </>
+);
 
 export default App;
