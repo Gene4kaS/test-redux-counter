cc46c5d7202ac0ed0d73a1a0a12039dbe5ae32fb
Gene4kaS <jannasavchenko@gmail.com>
2022-02-04 15:43:47 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-// import { BrouserRouter } from 'react-router-dom';
+import App from './App';
 import store from './redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <BrouserRouter> */}
-    <Provider store={store}>
-      <App />
-    </Provider>
-    {/* </BrouserRouter> */}
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
